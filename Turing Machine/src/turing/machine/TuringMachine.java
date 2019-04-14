098485e26400997bdf90e2101a58d4ad808cd15e
Hay00 <Hay00@users.noreply.github.com>
2019-04-14 01:32:25 -0300

    Implementação Tela

--- Turing Machine/src/turing/machine/TuringMachine.java
+++ Turing Machine/src/turing/machine/TuringMachine.java
@@ -6,11 +6,14 @@ public class TuringMachine {
 
     private int cabecote;
     private String estado = "";
-    private char[] fita = new char[6];
+    private char[] fita = new char[15];
     private String[][] tabela = new String[6][7];
+    private final String DIREITA = "direita";
+    private final String ESQUERDA = "esquerda";
+    private final char BRANCO = 'β';
 
     /*
-     Inicio gets e sets
+     * Inicio gets e sets
      */
     public int getCabecote() {
         return cabecote;
@@ -37,21 +40,23 @@ public class TuringMachine {
     }
 
     /*
-     Final gets e sets
+     * Final gets e sets
      */
     public void Inicializar(char[] entrada) {
-        if (entrada.length < 10) {
+        if (entrada.length < 18) {
             for (int count = 0; count < entrada.length; count++) {
                 fita[count + 1] = entrada[count];
             }
             setCabecote(0);
             setEstado("q0");
             fita[0] = ('*');
-            fita[5] = ('V');
-        } else {
-            throw new RuntimeException("Sua string contém mais de 6 caracteres!");
-        }
 
+            for (int count = entrada.length + 1; count < fita.length; count++) {
+                fita[count] = (BRANCO);
+            }
+        } else {
+            throw new RuntimeException("Sua string contém mais de 18 caracteres!");
+        }
     }
 
     public char Ler() {
@@ -68,98 +73,100 @@ public class TuringMachine {
     }
 
     public void Mover(String direcao) {
-        if (direcao.equals("direita")) {
-            setCabecote(cabecote + 1);
-        } else if (direcao.equals("esquerda")) {
-            setCabecote(cabecote - 1);
-        } else {
-            throw new RuntimeException("Direção incorreta!");
+        if (cabecote <= fita.length - 1) {
+            if (direcao.equals(DIREITA)) {
+                setCabecote(cabecote + 1);
+            } else if (direcao.equals(ESQUERDA)) {
+                setCabecote(cabecote - 1);
+            } else {
+                throw new RuntimeException("Direção incorreta!");
+            }
         }
     }
 
     public String q0(char palavra) {
         if (palavra == '*') {
             Gravar('*');
-            Mover("direita");
+            Mover(DIREITA);
             return "q0";
         } else if (palavra == 'B') {
             Gravar('B');
-            Mover("direita");
+            Mover(DIREITA);
             return "q3";
         } else if (palavra == 'a') {
             Gravar('A');
-            Mover("direita");
+            Mover(DIREITA);
             return "q1";
-        } else if (palavra == 'V') {
-            Gravar('V');
-            Mover("direita");
+        } else if (palavra == BRANCO) {
+            Gravar(BRANCO);
+            Mover(DIREITA);
             return "q4";
-        } else {
-            throw new RuntimeException("Palavra incorreta!");
         }
+        return "qe";
     }
 
     public String q1(char palavra) {
         if (palavra == 'a') {
             Gravar('a');
-            Mover("direita");
+            Mover(DIREITA);
             return "q1";
         } else if (palavra == 'B') {
             Gravar('B');
-            Mover("direita");
+            Mover(DIREITA);
             return "q1";
         } else if (palavra == 'b') {
             Gravar('B');
-            Mover("esquerda");
+            Mover(ESQUERDA);
             return "q2";
-        } else {
-            throw new RuntimeException("Palavra incorreta!");
         }
+        return "qe";
     }
 
     public String q2(char palavra) {
         if (palavra == 'a') {
             Gravar('a');
-            Mover("esquerda");
+            Mover(ESQUERDA);
             return "q2";
         } else if (palavra == 'B') {
             Gravar('B');
-            Mover("esquerda");
+            Mover(ESQUERDA);
             return "q2";
         } else if (palavra == 'A') {
             Gravar('A');
-            Mover("direita");
+            Mover(DIREITA);
             return "q0";
-        } else {
-            throw new RuntimeException("Palavra incorreta!");
         }
+        return "qe";
     }
 
     public String q3(char palavra) {
         if (palavra == 'B') {
             Gravar('B');
-            Mover("direita");
+            Mover(DIREITA);
             return "q3";
-        } else if (palavra == 'V') {
-            Gravar('V');
-            Mover("esquerda");
+        } else if (palavra == BRANCO) {
+            Gravar(BRANCO);
+            Mover(ESQUERDA);
             return "q4";
-        } else {
-            throw new RuntimeException("Palavra incorreta!");
         }
+        return "qe";
     }
 
     public String q4(char palavra) {
         return "Estado de Aceitação!";
     }
 
+    public String qe(char palavra) {
+        return "Palavra Rejeitada!";
+    }
+
     public static void main(String[] args) {
         TuringMachine maquina = new TuringMachine();
         Scanner teclado = new Scanner(System.in);
-        
+
         try {
-            System.out.println("Insira um valor");
-        
+            System.out.println("Insira uma palavra:");
+
             char[] entrada_teclado = teclado.next().toCharArray();
             teclado.close();
             for (int count = 0; count < entrada_teclado.length; count++) {
@@ -170,26 +177,7 @@ public class TuringMachine {
             maquina.Inicializar(entrada_teclado);
 
             while (true) {
-                char[] a = maquina.getFita();
 
-                char maq_letra = maquina.Ler();
-                System.out.println("Cabeçote: " + maquina.getCabecote());
-                System.out.println("Estado atual: " + maquina.getEstado());
-                for (int count = 0; count < a.length; count++) {
-                    System.out.println("Array: " + a[count]);
-                }
-                if (maquina.getEstado().equals("q0")) {
-                    maquina.setEstado(maquina.q0(maq_letra));
-                } else if (maquina.getEstado().equals("q1")) {
-                    maquina.setEstado(maquina.q1(maq_letra));
-                } else if (maquina.getEstado().equals("q2")) {
-                    maquina.setEstado(maquina.q2(maq_letra));
-                } else if (maquina.getEstado().equals("q3")) {
-                    maquina.setEstado(maquina.q3(maq_letra));
-                } else if (maquina.getEstado().equals("q4")) {
-                    System.out.println(maquina.q4(maq_letra));
-                    break;
-                }
             }
         } catch (RuntimeException e) {
             System.out.println(e);
